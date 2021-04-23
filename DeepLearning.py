8f0a61c93cf2bd92e2ed6c31106b35984e58609b
Cannon Lock <lockcannon@hotmail.com>
2021-04-23 17:29:05 -0500

    Temp

--- DeepLearning.py
+++ DeepLearning.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import torchtext
 import time
+import math
 import random
 import pandas as pd
 import numpy as np
@@ -10,21 +11,6 @@ import pickle
 from music21 import converter, instrument, note, chord
 
 GENRES = ["Blues", "Country", "Indie", "Jazz", "Pop", "Psychedelic Rock", "Rock", "Soul"]
-
-# TODO : (
-
-def train_network():
-	""" Train a Neural Network to generate music """
-	notes = get_notes()
-
-	# get amount of pitch names
-	n_vocab = len(set(notes))
-
-	network_input, network_output = prepare_sequences(notes, n_vocab)
-
-	model = create_network(network_input, n_vocab)
-
-	train(model, network_input, network_output)
 
 def get_note_str(note, duration):
 	return note.nameWithOctave + duration
@@ -91,12 +77,16 @@ def get_dataset(includeDuration = False, byGenre = False, regenerate = False):
 
 	return dataset
 
-def build_vocab(notes, includeDuration = False):
-	vocab = set()
+def build_vocab(songs, includeDuration = False):
+	vocab_set = set()
 	for i in songs:
-		vocab.update(i)
+		vocab_set.update(i)
 
-	return vocab
+	vocab_dict = {}
+	for index, element in enumerate(sorted(vocab_set)):
+		vocab_dict[element] = index
+
+	return vocab_dict
 
 def standardize_songs(songs):
 
@@ -116,77 +106,17 @@ def standardize_songs(songs):
 
 	return adjusted_songs
 
+def rand_song_slice(song, slice_number, slice_length):
+	start_element_i = math.floor(random.random() * (len(song) - slice_length - 1))
+	end_element_i = start + slice_length
+	return song[start_element, end_element_i]
 
-def prepare_sequences(notes):
-	""" Prepare the sequences used by the Neural Network """
-	sequence_length = 100
-	vocab_length = len(set(notes))
+def slice_to_tensor(vocab, slice):
+	out = torch.zeros(len(seq)).long()
 
-	# get all pitch names
-	pitchnames = sorted(set(notes))
-
-	# create a dictionary to map pitches to integers
-	note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-
-	network_input = []
-	network_output = []
-
-	# create input sequences and the corresponding outputs
-	for i in range(0, len(notes) - sequence_length, 1):
-		sequence_in = notes[i:i + sequence_length]
-		sequence_out = notes[i + sequence_length]
-		network_input.append([note_to_int[char] for char in sequence_in])
-		network_output.append(note_to_int[sequence_out])
-
-	n_patterns = len(network_input)
-
-	# reshape the input into a format compatible with LSTM layers
-	network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
-	# normalize input
-	network_input = network_input / float(vocab_length)
-
-	network_output = np_utils.to_categorical(network_output)
-
-	return (network_input, network_output)
-
-def create_network(network_input, n_vocab):
-	""" create the structure of the neural network """
-	model = Sequential()
-	model.add(LSTM(
-		512,
-		input_shape=(network_input.shape[1], network_input.shape[2]),
-		recurrent_dropout=0.3,
-		return_sequences=True
-	))
-	model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
-	model.add(LSTM(512))
-	model.add(BatchNorm())
-	model.add(Dropout(0.3))
-	model.add(Dense(256))
-	model.add(Activation('relu'))
-	model.add(BatchNorm())
-	model.add(Dropout(0.3))
-	model.add(Dense(n_vocab))
-	model.add(Activation('softmax'))
-	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
-	return model
-
-def train(model, network_input, network_output):
-	""" train the neural network """
-	filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
-	checkpoint = ModelCheckpoint(
-		filepath,
-		monitor='loss',
-		verbose=0,
-		save_best_only=True,
-		mode='min'
-	)
-	callbacks_list = [checkpoint]
-
-	model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
 
 if __name__ == '__main__':
-	songs = get_dataset(regenerate=True)
+	songs = get_dataset()
+	vocab = build_vocab(songs['total'])
 	l = standardize_songs(songs["total"])
 	print("Fart")
